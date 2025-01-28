77fc9e2bd28d8f33af124134c57e785a7887b5f4
ZeroTB01 <117263881+ZeroTB01@users.noreply.github.com>
2025-01-28 17:06:39 +0800

    Add files via upload

---
+++ Main/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 4;
+        int sum = add(num1,num2);
+        System.out.println(sum);
+    }
+    public static int add(int a,int b){
+        return a+b;
+    }
+}
